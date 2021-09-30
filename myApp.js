7a43450b4c62d3d93f5b67a03ddca1328f7d30c3
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2021-09-30 12:30:57 -0400

    finepeoplename

--- myApp.js
+++ myApp.js
@@ -39,7 +39,11 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({name: personName}, function(err, data)
+  {
+    done(null , data);
+  });
+  
 };
 
 const findOneByFood = (food, done) => {
