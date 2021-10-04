578a0c96c0435aedf4d69c39b9815675cc35881c
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2021-10-04 13:37:30 -0400

    descending idk!

--- myApp.js
+++ myApp.js
@@ -125,7 +125,7 @@ const queryChain = (done) => {
   const foodToSearch = "burrito";
 
   Person.find({favoriteFoods: foodToSearch})
-  .sort({name: 1})
+  .sort({name: -1})
   .limit(2)
   .select('-name')
   .exec((err, data) => 
